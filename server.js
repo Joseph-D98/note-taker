a6886d718775d596e6dd6c28ba6f753d4d1d350b
Joseph-D98 <defelicejoseph@outlook.com>
2020-10-10 18:19:02 -0600

    delete notes

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 9080;
 
 // sets up data parsing and reading static files
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
@@ -19,6 +21,7 @@ app.use(express.static(__dirname + "/public"));
 
 // global array using promise to read the JSON 
 let savedNotesGlobal = util.promisify(fs.readFile)
+
 function getSavedNotes() {
   console.log("Saved Notes", savedNotesGlobal("./db/db.json", "utf8"))
   return savedNotes = savedNotesGlobal("./db/db.json", "utf8")
@@ -26,34 +29,51 @@ function getSavedNotes() {
 
 // returns all saved notes as JSON
 app.get("/api/notes", (req, res) => {
-  getSavedNotes() 
-    .then((savedNotes) => { 
-    res.send(JSON.parse(savedNotes))})  
-    .catch((err) => res.status(500).json(err)); 
-  });
+  getSavedNotes()
+    .then((savedNotes) => {
+      res.send(JSON.parse(savedNotes))
+    })
+    .catch((err) => res.status(500).json(err));
+});
 
 // saves new note to db.json and returns note to client
 app.post("/api/notes", (req, res) => {
-  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json")); 
-  let id = crypto.randomBytes(16).toString("hex");  
-  let newNote = { 
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+  let id = crypto.randomBytes(16).toString("hex");
+  let newNote = {
     title: req.body.title,
-    text: req.body.text, 
+    text: req.body.text,
     id: id,
   }
   console.log("newNote:", newNote)
 
   // pushes new notes to notes.index
   savedNotes.push(newNote);
-  
+
   // writes new notes to db.json
   fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes), (err) => {
-    if (err) throw err; 
+    if (err) throw err;
     console.log("error");
   });
-    console.log("A note new has been written");
-    return res.json(savedNotes);
+  console.log("A note new has been written");
+  return res.json(savedNotes);
+});
+
+// Receives query parameter containing ID of the note to delete. 
+app.delete("/api/notes/:id", (req, res) => {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json")); // reads db.json
+  let noteID = savedNotes.filter(x => x.id != req.params.id) // returns route with all notes EXCEPT the ID we are deleting 
+  console.log("NOTE ID", noteID)
+  console.log("REQ.PARAMS.ID", req.params.id)
+
+  // writes new notes to db.json
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteID), (err) => {
+    if (err) throw err;
+    console.log("error");
   });
+  console.log("Your note has been deleted");
+  return res.json(savedNotes);
+});
 
 
 
